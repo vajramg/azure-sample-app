9e2880fd5f9339f4367fd7d0bf57eb7c38b25a1c
vajramg <vajramg@gmail.com>
2026-05-31 16:43:17 +0530

    added keyvault

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -1,12 +1,3 @@
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
-
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "vault" {
@@ -17,6 +8,7 @@ resource "azurerm_key_vault" "vault" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
+
 
   sku_name = "standard"
 
